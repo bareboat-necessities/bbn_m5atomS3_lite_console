8f26ee1f942123346463f91b129411f12ac913a2
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-12-24 13:41:08 -0500

    Update bbn_m5atomS3_lite_console.ino

--- bbn_m5atomS3_lite_console.ino
+++ bbn_m5atomS3_lite_console.ino
@@ -1,12 +1,4 @@
-
-/* Basic console example (esp_console_repl API)
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+#include <M5AtomS3.h>
 
 #include <stdio.h>
 #include <string.h>
