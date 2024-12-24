3d85a1d2a0f7933e89c96fff55cdce3cf7915dab
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-12-24 13:23:12 -0500

    Update bbn_m5atomS3_lite_console.ino

--- bbn_m5atomS3_lite_console.ino
+++ bbn_m5atomS3_lite_console.ino
@@ -103,7 +103,7 @@ void setup(void)
 #endif
 //    register_nvs();
 
-#elif defined(CONFIG_ESP_CONSOLE_USB_CDC)
+#if defined(CONFIG_ESP_CONSOLE_USB_CDC)
     esp_console_dev_usb_cdc_config_t hw_config = ESP_CONSOLE_DEV_CDC_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_console_new_repl_usb_cdc(&hw_config, &repl_config, &repl));   
 #else
