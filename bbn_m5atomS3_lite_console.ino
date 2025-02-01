1336c42805f147d8a97840e40fd2801e7c182221
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-01 00:12:28 -0500

    Update bbn_m5atomS3_lite_console.ino

--- bbn_m5atomS3_lite_console.ino
+++ bbn_m5atomS3_lite_console.ino
@@ -5,19 +5,25 @@ using namespace ESP32Console;
 
 Console console;
 
-void setup() {   
-    //Initialize Serial port on UART0 (the onboard USB Serial Converter)
-    Serial.begin(115200);
-    
-    //Enable ESP32Console on Pin 12 & 14 on UART1
-    console.begin(115200, 12, 14, 1);
+void setup() {
+  auto cfg = M5.config();
+  AtomS3.begin(cfg);
 
-    console.registerSystemCommands();
-    //console.registerNetworkCommands();
+  //Serial2.begin(9600, SERIAL_8N1, G6, G5);  // UART pins when GPS is plugged into port C of ATOMIC PortABC Extension Base
+  //Serial2.begin(9600, SERIAL_8N1, G1, G2);  // UART pins when GPS is plugged directly into grove port of M5 AtomS3-lite
+
+  // Initialize Serial port on UART0 (the onboard USB Serial Converter)
+  Serial.begin(115200);
+
+  // Enable ESP32Console on Pin 6 & 5 on UART1
+  console.begin(9600, G6, G5, 1);
+
+  console.registerSystemCommands();
+  //console.registerNetworkCommands();
 }
 
 void loop() {
-    //We can use Serial on UART0 as usual, while ESP32Console is available on the Pins above.
-    Serial.println("Test");
-    delay(1000);
+  // We can use Serial on UART0 as usual, while ESP32Console is available on the Pins above.
+  Serial.println("Test");
+  delay(1000);
 }
