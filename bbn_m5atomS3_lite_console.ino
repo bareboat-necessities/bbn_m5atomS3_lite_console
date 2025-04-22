bd20991a940b418123c267cb936e68d2ac172ccb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-04-22 14:37:34 -0400

    Update bbn_m5atomS3_lite_console.ino

--- bbn_m5atomS3_lite_console.ino
+++ bbn_m5atomS3_lite_console.ino
@@ -1,172 +1,181 @@
 #define CONFIG_ESP_CONSOLE_USB_CDC 1
 
-#include <Arduino.h>
-#include <USB.h>
-#include <Esp.h>
+/*
+   SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
+
+   SPDX-License-Identifier: Unlicense OR CC0-1.0
+*/
+/* USB console example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <esp_system.h>
+#include <esp_log.h>
 #include <esp_console.h>
 #include <linenoise/linenoise.h>
 #include <argtable3/argtable3.h>
+#include <esp_vfs_cdcacm.h>
+#include <nvs.h>
+#include <nvs_flash.h>
+//#include <cmd_nvs.h>
+//#include <cmd_system.h>
+//#include <cmd_wifi.h>
 
-// Configuration
-#define MAX_LINE_LENGTH 256
-#define MAX_HISTORY 50
-static const char* PROMPT = "\033[1;32mesp32s3>\033[0m\r ";
-//static const char* PROMPT = "";
 
-// Function prototypes
-static int help_command(int argc, char** argv);
-static int version_command(int argc, char** argv);
-static int echo_command(int argc, char** argv);
-static int reboot_command(int argc, char** argv);
-void register_commands();
-void console_task(void* parameter);
+/* 'heap' command prints minumum heap size */
+static int heap_size(int argc, char **argv) {
+  uint32_t heap_size = heap_caps_get_minimum_free_size(MALLOC_CAP_DEFAULT);
+  printf("min heap size: %u\n", heap_size);
+  return 0;
+}
+
+static void register_heap(void) {
+  const esp_console_cmd_t heap_cmd = {
+    .command = "heap",
+    .help = "Get minimum size of free heap memory that was available during program execution",
+    .hint = NULL,
+    .func = &heap_size,
+  };
+  ESP_ERROR_CHECK( esp_console_cmd_register(&heap_cmd) );
+
+}
+
+/*
+   We warn if a secondary serial console is enabled. A secondary serial console is always output-only and
+   hence not very useful for interactive console applications. If you encounter this warning, consider disabling
+   the secondary serial console in menuconfig unless you know what you are doing.
+*/
+#if SOC_USB_SERIAL_JTAG_SUPPORTED
+#if !CONFIG_ESP_CONSOLE_SECONDARY_NONE
+#warning "A secondary serial console is not useful when using the console component. Please disable it in menuconfig."
+#endif
+#endif
+
+static void initialize_nvs(void) {
+  esp_err_t err = nvs_flash_init();
+  if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK( nvs_flash_erase() );
+    err = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(err);
+}
+
+static void initialize_console(void) {
+  /* Disable buffering on stdin */
+  setvbuf(stdin, NULL, _IONBF, 0);
+
+  /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
+  esp_vfs_dev_cdcacm_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+  /* Move the caret to the beginning of the next line on '\n' */
+  esp_vfs_dev_cdcacm_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
+
+  /* Enable non-blocking mode on stdin and stdout */
+  fcntl(fileno(stdout), F_SETFL, 0);
+  fcntl(fileno(stdin), F_SETFL, 0);
+
+  /* Initialize the console */
+  esp_console_config_t console_config = {
+    .max_cmdline_length = 256,
+    .max_cmdline_args = 8,
+#if CONFIG_LOG_COLORS
+    .hint_color = atoi(LOG_COLOR_CYAN)
+#endif
+  };
+  ESP_ERROR_CHECK( esp_console_init(&console_config) );
+
+  /* Configure linenoise line completion library */
+  /* Enable multiline editing. If not set, long commands will scroll within
+     single line.
+  */
+  linenoiseSetMultiLine(1);
+
+  /* Tell linenoise where to get command completions and hints */
+  linenoiseSetCompletionCallback(&esp_console_get_completion);
+  linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
+
+  /* Set command history size */
+  linenoiseHistorySetMaxLen(10);
+}
+
+/* Prompt to be printed before each line.
+   This can be customized, made dynamic, etc.
+*/
+const char* prompt = LOG_COLOR_I CONFIG_IDF_TARGET "> " LOG_RESET_COLOR;
 
 void setup() {
-  // Start USB serial (CDC)
-  //USB.begin();
-  Serial.begin(115200);
-  while (!Serial) delay(10);
+  initialize_nvs();
 
-  // Initialize console
-  esp_console_config_t console_config = ESP_CONSOLE_DEV_CDC_CONFIG_DEFAULT(); //ESP_CONSOLE_CONFIG_DEFAULT();
-  console_config.max_cmdline_length = MAX_LINE_LENGTH;
-  ESP_ERROR_CHECK(esp_console_init(&console_config));
+  initialize_console();
 
-  // Configure line editing library
-  linenoiseSetMultiLine(0);
-  linenoiseSetCompletionCallback(&esp_console_get_completion);
-  linenoiseAllowEmpty(false);
-  linenoiseHistorySetMaxLen(MAX_HISTORY);
-  //linenoiseSetPrompt(PROMPT);
+  /* Register commands */
+  esp_console_register_help_command();
+  //register_system_common();
+#if SOC_LIGHT_SLEEP_SUPPORTED
+  //register_system_light_sleep();
+#endif
+#if SOC_DEEP_SLEEP_SUPPORTED
+  //register_system_deep_sleep();
+#endif
+#if SOC_WIFI_SUPPORTED
+  //register_wifi();
+#endif
+  //register_nvs();
+  register_heap();
 
-  // Register commands
-  register_commands();
+  printf("\n"
+         "This is an example of ESP-IDF console component.\n"
+         "Type 'help' to get the list of commands.\n"
+         "Use UP/DOWN arrows to navigate through command history.\n"
+         "Press TAB when typing command name to auto-complete.\n");
 
-  // Start console task
-  xTaskCreate(console_task, "console_task", 4096, NULL, 1, NULL);
-
-  Serial.println("\nESP32-S3 USB Console Ready (Arduino-ESP32 3.1)");
-  Serial.println("Type 'help' for command list");
+  /* Figure out if the terminal supports escape sequences */
+  int probe_status = linenoiseProbe();
+  if (probe_status) { /* zero indicates success */
+    printf("\n"
+           "Your terminal application does not support escape sequences.\n"
+           "Line editing and history features are disabled.\n"
+           "On Windows, try using Putty instead.\n");
+    linenoiseSetDumbMode(1);
+#if CONFIG_LOG_COLORS
+    /* Since the terminal doesn't support escape sequences,
+       don't use color codes in the prompt.
+    */
+    prompt = CONFIG_IDF_TARGET "> ";
+#endif //CONFIG_LOG_COLORS
+  }
 }
 
 void loop() {
-  // Main loop can be used for other tasks
-  delay(1000);
-}
-
-// Console task running in background
-void console_task(void* parameter) {
-  while (true) {
-    if (Serial.available() <= 0) {
-      continue;
-    }
-    // Get command line
-    char* line = linenoise(PROMPT); //linenoise(linenoiseGetPrompt());
-    if (line == NULL) {
-      vTaskDelay(10 / portTICK_PERIOD_MS);
-      continue;
-    }
-    //Serial.print(line);
-
-    // Add to history
-    if (strlen(line) > 0) {
-      linenoiseHistoryAdd(line);
-    }
-
-    // Execute command
-    int ret;
-    esp_err_t err = esp_console_run(line, &ret);
-    if (err == ESP_ERR_NOT_FOUND) {
-      //Serial.println("Error: Unrecognized command");
-    } else if (err == ESP_ERR_INVALID_ARG) {
-      // Empty command
-    } else if (err == ESP_OK && ret != ESP_OK) {
-      Serial.printf("Command failed (0x%x)\n", ret);
-    } else if (err != ESP_OK) {
-      Serial.printf("Internal error: %s\n", esp_err_to_name(err));
-    }
-
-    int len = strlen(line);
-    while (Serial.available() > 0) {
-      char c = Serial.read();
-      //Serial.printf("%c\n", c);
-    }
-    // Free memory
-    linenoiseFree(line);
-    //linenoisePrintPrompt();
+  /* Get a line using linenoise.
+     The line is returned when ENTER is pressed.
+  */
+  char* line = linenoise(prompt);
+  if (line == NULL) { /* Ignore empty lines */
+    return;
   }
-}
+  /* Add the command to the history */
+  linenoiseHistoryAdd(line);
 
-// ======= Command Implementations ======= //
-
-static int help_command(int argc, char** argv) {
-  Serial.printf("Help\n");
-  //esp_console_print_help();
-  return ESP_OK;
-}
-
-static int version_command(int argc, char** argv) {
-  //Serial.printf("Arduino-ESP32 Version: %s\n", ESP.getArduinoVersion());
-  Serial.printf("SDK Version: %s\n", ESP.getSdkVersion());
-  Serial.printf("Chip Model: %s\n", ESP.getChipModel());
-  Serial.printf("CPU Cores: %d\n", ESP.getChipCores());
-  Serial.printf("Flash Size: %dMB\n", ESP.getFlashChipSize() / (1024 * 1024));
-  Serial.printf("PSRAM Size: %dMB\n", ESP.getPsramSize() / (1024 * 1024));
-  return ESP_OK;
-}
-
-static int echo_command(int argc, char** argv) {
-  for (int i = 1; i < argc; i++) {
-    if (i > 1) Serial.print(" ");
-    Serial.print(argv[i]);
+  /* Try to run the command */
+  int ret;
+  esp_err_t err = esp_console_run(line, &ret);
+  if (err == ESP_ERR_NOT_FOUND) {
+    printf("Unrecognized command\n");
+  } else if (err == ESP_ERR_INVALID_ARG) {
+    // command was empty
+  } else if (err == ESP_OK && ret != ESP_OK) {
+    printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(ret));
+  } else if (err != ESP_OK) {
+    printf("Internal error: %s\n", esp_err_to_name(err));
   }
-  Serial.println();
-  return ESP_OK;
-}
-
-static int reboot_command(int argc, char** argv) {
-  Serial.println("Rebooting...");
-  delay(100);
-  ESP.restart();
-  return ESP_OK;
-}
-
-// ======= Command Registration ======= //
-
-void register_commands() {
-  const esp_console_cmd_t help_cmd = {
-    .command = "help",
-    .help = "Print list of commands",
-    .hint = NULL,
-    .func = &help_command,
-    .argtable = NULL
-  };
-  esp_console_cmd_register(&help_cmd);
-
-  const esp_console_cmd_t version_cmd = {
-    .command = "version",
-    .help = "Get system version information",
-    .hint = NULL,
-    .func = &version_command,
-    .argtable = NULL
-  };
-  esp_console_cmd_register(&version_cmd);
-
-  const esp_console_cmd_t echo_cmd = {
-    .command = "echo",
-    .help = "Echo the input arguments",
-    .hint = NULL,
-    .func = &echo_command,
-    .argtable = NULL
-  };
-  esp_console_cmd_register(&echo_cmd);
-
-  const esp_console_cmd_t reboot_cmd = {
-    .command = "reboot",
-    .help = "Reboot the system",
-    .hint = NULL,
-    .func = &reboot_command,
-    .argtable = NULL
-  };
-  esp_console_cmd_register(&reboot_cmd);
+  /* linenoise allocates line buffer on the heap, so need to free it */
+  linenoiseFree(line);
 }
